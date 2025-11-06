0372a36ecb96270efc371aed7f29bb4f77fd6307
Timofey Kochetov <herew26@gmail.com>
2025-11-06 11:28:22 +0300

    ✨ Add link to rules

--- src/pages/PrizesPage.tsx
+++ src/pages/PrizesPage.tsx
@@ -4,6 +4,21 @@ import { Page } from "@/components/Page";
 import { PromocodesSection } from "@/components/PromocodesSection";
 import { StatsSection } from "@/components/StatsSection";
 import { List } from "@telegram-apps/telegram-ui";
+import { openLink } from "@telegram-apps/sdk-react";
+
+const PromocodeRulesLink: React.FC = () => (
+    <a
+        style={{
+            color: "var(--tgui--link_color)",
+            cursor: "pointer",
+            textDecoration: "underline",
+            fontWeight: "normal",
+        }}
+        onClick={() => openLink("https://legal.mm.ru/")}
+    >
+        Правила промокодов
+    </a>
+);
 
 const PrizesPage: React.FC = () => {
     return (
@@ -13,13 +28,25 @@ const PrizesPage: React.FC = () => {
 
                 <PromocodesSection
                     header="Промокоды за рекорд"
-                    footer="Получите призы за лучший результат в одной игре. Чем выше ваш рекорд, тем ценнее награда!"
+                    footer={
+                        <>
+                            Получите призы за лучший результат в одной игре. Чем
+                            выше ваш рекорд, тем ценнее награда!{" "}
+                            <PromocodeRulesLink />
+                        </>
+                    }
                     type="record"
                 />
 
                 <PromocodesSection
                     header="Промокоды за все игры"
-                    footer="Получите призы за суммарное количество очков за все игры. Чем больше очков, тем ценнее награда!"
+                    footer={
+                        <>
+                            Получите призы за суммарное количество очков за все
+                            игры. Чем больше очков, тем ценнее награда!{" "}
+                            <PromocodeRulesLink />
+                        </>
+                    }
                     type="total"
                 />
             </List>
